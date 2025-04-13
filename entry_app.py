3b5c692eabb670d343dd08e7bc6bc7846fa13ae6
oneway215 <leeilroo27@gmail.com>
2025-04-13 14:44:34 +0900

    Create entry_app.py

--- entry_app.py
+++ entry_app.py
@@ -30,4 +30,4 @@ def check_in():
     return render_template('entry.html', user_id=user_id, seat_id=seat_id)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
